2a3989e9888ab09619eb1b0b8ad0106422fdf6ee
Armengaud Kevin <kevarmengaud@gmail.com>
2020-05-01 15:32:59 +0200

    Add user experience code and prepare connection with arduino

--- app/script.js
+++ app/script.js
@@ -1,231 +1,268 @@
-var reglage1 = 0;
-var reglage2 = 0;
-var reglage3 = 0;
-var reglage4 = 0;
-var reglage5 = 0;
-var reglage6 = 0;
-var reglage7 = 0;
-var reglage8 = 0;
-var reglage9 = 0;
-var reglage10 = 0;
-var reglage11 = 0;
-var reglage12 = 0;
-var reglage13 = 0;
-var reglage14 = 0;
-var reglage15 = 0;
-var reglage16 = 0;
-var reglage17 = 0;
-var reglage18 = 0;
-var reglage19 = 0;
+var reglage = ["0", "0", "0", "0", "100", "0", "0", "0", "0", "0", "0", "0"];
+var nomBtn = "";
+var idSelected = "";
 
 const os = require('os');
-const storage = require('electron-json-storage');
-const dataPath = storage.getDataPath();
+
+const Store = require('electron-store');
+const store = new Store();
+
 window.$ = window.jQuery = require('jquery');
 
 $('#slider1').on("change mousemove", function () {
-    reglage1 = $(this).val();
+    reglage[0] = $(this).val();
     $('#valeurReglage1').html($(this).val());
 
 });
 
 $('#slider2').on("change mousemove", function () {
-    reglage2 = $(this).val();
+    reglage[1] = $(this).val();
     $('#valeurReglage2').html($(this).val());
 
 });
 
 $('#slider3').on("change mousemove", function () {
-    reglage3 = $(this).val();
+    reglage[2] = $(this).val();
     $('#valeurReglage3').html($(this).val());
 
 });
 
 $('#slider4').on("change mousemove", function () {
-    reglage4 = $(this).val();
+    reglage[3] = $(this).val();
     $('#valeurReglage4').html($(this).val());
 
 });
 
 $('#slider5').on("change mousemove", function () {
-    reglage5 = $(this).val();
+    reglage[4] = $(this).val();
     $('#valeurReglage5').html($(this).val());
 
 });
 
 $('#slider6').on("change mousemove", function () {
-    reglage6 = $(this).val();
+    reglage[5] = $(this).val();
     $('#valeurReglage6').html($(this).val());
 
 });
 
 $('#slider7').on("change mousemove", function () {
-    reglage7 = $(this).val();
+    reglage[6] = $(this).val();
     $('#valeurReglage7').html($(this).val());
 
 });
 
 $('#slider8').on("change mousemove", function () {
-    reglage8 = $(this).val();
+    reglage[7] = $(this).val();
     $('#valeurReglage8').html($(this).val());
 
 });
 
 $('#slider9').on("change mousemove", function () {
-    reglage9 = $(this).val();
+    reglage[8] = $(this).val();
     $('#valeurReglage9').html($(this).val());
 
 });
 
 $('#slider10').on("change mousemove", function () {
-    reglage10 = $(this).val();
+    reglage[9] = $(this).val();
     $('#valeurReglage10').html($(this).val());
 
 });
 
 $('#slider11').on("change mousemove", function () {
-    reglage11 = $(this).val();
+    reglage[10] = $(this).val();
     $('#valeurReglage11').html($(this).val());
 
 });
 
 $('#slider12').on("change mousemove", function () {
-    reglage12 = $(this).val();
+    reglage[11] = $(this).val();
     $('#valeurReglage12').html($(this).val());
 
 });
 
-$('#slider13').on("change mousemove", function () {
-    reglage13 = $(this).val();
-    $('#valeurReglage13').html($(this).val());
+function btnInactive() {
+    $('#btn1').removeClass('active');
+    $('#btn2').removeClass('active');
+    $('#btn3').removeClass('active');
+    $('#btn4').removeClass('active');
+    $('#btn5').removeClass('active');
+    $('#btn6').removeClass('active');
+    $('#btn7').removeClass('active');
+    $('#btn8').removeClass('active');
+    $('#btn9').removeClass('active');
+};
 
+function loadAction(idBtn) {
+    $('#listeAction').empty();
+    console.log(store.get(idBtn));
+    var listeAction = store.get(idBtn);
+
+    if (listeAction != undefined) {
+        var option = "";
+        for (i = 0; i < Object.keys(listeAction).length; i++) {
+            option = "<option value=" + Object.keys(listeAction)[i] + ">" + Object.keys(listeAction)[i] + "</option>";
+            $("#listeAction").append(option);
+        }
+    }
+};
+
+
+$('#btn1').on("click", function () {
+    btnInactive();
+    $('#btn1').addClass('active');
+    nomBtn = this.id;
+    loadAction(nomBtn);
 });
 
-$('#slider14').on("change mousemove", function () {
-    reglage14 = $(this).val();
-    $('#valeurReglage14').html($(this).val());
-
+$('#btn2').on("click", function () {
+    btnInactive();
+    $('#btn2').addClass('active');
+    nomBtn = this.id;
+    loadAction(nomBtn);
 });
 
-$('#slider15').on("change mousemove", function () {
-    reglage15 = $(this).val();
-    $('#valeurReglage15').html($(this).val());
-
+$('#btn3').on("click", function () {
+    btnInactive();
+    $('#btn3').addClass('active');
+    nomBtn = this.id;
+    loadAction(nomBtn);
 });
 
-$('#slider16').on("change mousemove", function () {
-    reglage16 = $(this).val();
-    $('#valeurReglage16').html($(this).val());
-
+$('#btn4').on("click", function () {
+    btnInactive();
+    $('#btn4').addClass('active');
+    nomBtn = this.id;
+    loadAction(nomBtn);
 });
 
-$('#slider17').on("change mousemove", function () {
-    reglage17 = $(this).val();
-    $('#valeurReglage17').html($(this).val());
-
+$('#btn5').on("click", function () {
+    btnInactive();
+    $('#btn5').addClass('active');
+    nomBtn = this.id;
+    loadAction(nomBtn);
 });
 
-$('#slider18').on("change mousemove", function () {
-    reglage18 = $(this).val();
-    $('#valeurReglage18').html($(this).val());
-
+$('#btn6').on("click", function () {
+    btnInactive();
+    $('#btn6').addClass('active');
+    nomBtn = this.id;
+    loadAction(nomBtn);
 });
 
-$('#slider19').on("change mousemove", function () {
-    reglage19 = $(this).val();
-    $('#valeurReglage19').html($(this).val());
-
+$('#btn7').on("click", function () {
+    btnInactive();
+    $('#btn7').addClass('active');
+    nomBtn = this.id;
+    loadAction(nomBtn);
 });
 
+$('#btn8').on("click", function () {
+    btnInactive();
+    $('#btn8').addClass('active');
+    nomBtn = this.id;
+    loadAction(nomBtn);
+});
+
+$('#btn9').on("click", function () {
+    btnInactive();
+    $('#btn9').addClass('active');
+    nomBtn = this.id;
+    loadAction(nomBtn);
+});
+
+$("#listeAction").on('change', function () {
+    idSelected = $(this).val();
+    var listeAction = store.get(nomBtn + "." + idSelected);
+    console.log(nomBtn + "." + idSelected);
+    console.log(idSelected);
+    for (i = 0; i < Object.keys(listeAction).length; i++) {
+        var idSlider = "#slider" + (i + 1);
+        var valeur = parseInt(listeAction[Object.keys(listeAction)[i]]);
+        reglage[i] = valeur;
+        $(idSlider).val(valeur);
+        $(idSlider).trigger('change');
+
+    }
+});
 
 $('#ajouterAction').on("click", function () {
+    var nomAction = $("#titreAction").val();
 
-    var listeAction ={};
+    if (nomBtn == "") {
+        alert("Selectionnez une scène.");
+    } else {
+        if (nomAction != "") {
+            var nomExistant = store.get(nomBtn + "." + nomAction);
+            var cheminJSON = nomBtn + "." + nomAction + ".";
 
-    //TODO check si nom est ok
+            if (nomExistant != undefined) {
+                if (confirm('Une action porte deja ce nom. Voulez vous la remplacer ?')) {
+                    for (i = 0; i < 12; i++) {
+                        store.set(cheminJSON + "reglage" + (i + 1), reglage[i]);
+                    }
+                    alert("Action remplacée.");
+                  } else {
 
-    storage.set( "nomaction", {
-        reglage1: reglage1,
-        reglage2: reglage2,
-        reglage3: reglage3,
-        reglage4: reglage4,
-        reglage5: reglage5,
-        reglage6: reglage6,
-        reglage7: reglage7,
-        reglage8: reglage8,
-        reglage9: reglage9,
-        reglage10: reglage10,
-        reglage11: reglage11,
-        reglage12: reglage12,
-        reglage13: reglage13,
-        reglage14: reglage14,
-        reglage15: reglage15,
-        reglage16: reglage16,
-        reglage17: reglage17,
-        reglage18: reglage18,
-        reglage19: reglage19
-    }, function (error) {
-        if (error) throw error;
-    });
-
-    $('#listeAction').append(new Option("nom action", "nom action"))
-
-    storage.has('listeAction', function(error, hasKey) {
-        if (error) throw error;
-      
-        if (hasKey) {
-            storage.get('listeAction', function (error, data) {
-                if (error) throw error;
-    
-                listeAction = data;
-                console.log("ListeAction1 :");
-                console.log(listeAction);
-            });
-
-            storage.set('listeAction', {
-                listeAction,
-            }, function (error) {
-                if (error) throw error;
-            });
-        }else{
-            storage.set('listeAction', {
-                1: "nom action",
-            }, function (error) {
-                if (error) throw error;
-            });
+                  }
+            } else {
+                for (i = 0; i < 19; i++) {
+                    store.set(cheminJSON + "reglage" + (i + 1), reglage[i]);
+                }
+                $('#listeAction').append(new Option(nomAction, nomAction));
+            }
+        } else {
+            alert("Rentrez un nom d'action.");
         }
-      });
-
-
-    storage.getAll(function(error, data) {
-        if (error) throw error;
-      
-        console.log("getALL");
-        console.log(data);
-        console.log("ListeAction");
-        console.log(listeAction);
-    });
-
+    }
 });
 
 $('#supprimerAction').on("click", function () {
-    
-    storage.clear(function(error) {
-        if (error) throw error;
-        console.log("CLEAR");
+    if (idSelected == "") {
+        alert("Selectionnez une action à supprimer.");
+    } else {
+        store.delete(nomBtn + "." + idSelected);
+        $("#listeAction option:selected").remove();
+        alert("L'action supprimée.");
+    }
+});
 
-      });
-    
-    storage.getAll(function(error, data) {
-        if (error) throw error;
-      
-        console.log("getALL");
-        console.log(data);
-        console.log("ListeAction");
-        console.log(listeAction);
-        console.log("XXXXXXXXXXXXXXXXXXXXX");
+$('#btnEffacer').on("click", function () {
+    if (nomBtn == "") {
+        alert("Selectionnez une scène à reset.");
+    } else {
+        store.delete(nomBtn);
+        $('#listeAction').empty();
+        alert("Scène effacée !");
+    }
+});
 
-    });    
+$('#jouerAction').on("click", function () {
+    play(reglage);
+});
 
+$('#jouerScene').on("click", function () {
+    var listeAction = store.get(nomBtn);
 
-})
+    if (listeAction != undefined) {
+        for (i = 0; i < Object.keys(listeAction).length; i++) {
+            var action = store.get(nomBtn + "." + Object.keys(listeAction)[i]);
+            for (i = 0; i < Object.keys(action).length; i++) {
+                var idSlider = "#slider" + (i + 1);
+                var valeur = parseInt(action[Object.keys(action)[i]]);
+                reglage[i] = valeur;
+                $(idSlider).val(valeur);
+                $(idSlider).trigger('change');
+
+            }
+            play(reglage);
+        }
+    } else {
+        alert("La scène que vous essayez de jouer est vide.");
+    }
+});
+
+function play(tableauReaglage) {
+    //Le tableau est un array
+    //TODO
+};
